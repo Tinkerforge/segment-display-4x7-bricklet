b911b176357e139d1b36e000e066c3d96bced69c
Matthias Bolte <matthias@tinkerforge.com>
2014-08-13 11:58:46 +0200

    Fix whitespace

--- software/examples/python/example_simple.py
+++ software/examples/python/example_simple.py
@@ -9,8 +9,6 @@ DIGITS = [0x3f,0x06,0x5b,0x4f,
 	      0x7f,0x6f,0x77,0x7c,
 	      0x39,0x5e,0x79,0x71] # // 0~9,A,b,C,d,E,F
 
-
-
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_segment_display_4x7 import SegmentDisplay4x7
 
