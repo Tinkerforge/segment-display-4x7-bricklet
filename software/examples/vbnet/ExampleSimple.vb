616a04df736f23fdc5e719efd58e5c5001d8ecf6
Matthias Bolte <matthias@tinkerforge.com>
2015-02-13 17:15:01 +0100

    vbnet: Use ReadLine instead of ReadKey
    Because ReadKey doesn't work with a pipe as stdin on the RED Brick.

--- software/examples/vbnet/ExampleSimple.vb
+++ software/examples/vbnet/ExampleSimple.vb
@@ -4,10 +4,10 @@ Module ExampleSimple
     Const HOST As String = "localhost"
     Const PORT As Integer = 4223
     Const UID As String = "XYZ" ' Change to your UID
-	Dim DIGITS() As Byte = {&H3f,&H06,&H5b,&H4f, _
-		                    &H66,&H6d,&H7d,&H07, _
-							&H7f,&H6f,&H77,&H7c, _
-							&H39,&H5e,&H79,&H71} ' 0~9,A,b,C,d,E,F
+    Dim DIGITS() As Byte = {&H3f,&H06,&H5b,&H4f, _
+                            &H66,&H6d,&H7d,&H07, _
+                            &H7f,&H6f,&H77,&H7c, _
+                            &H39,&H5e,&H79,&H71} ' 0~9,A,b,C,d,E,F
 
     Sub Main()
         Dim ipcon As New IPConnection() ' Create IP connection
@@ -17,11 +17,11 @@ Module ExampleSimple
         ' Don't use device before ipcon is connected
 
         ' Write "4223" to the display with full brightness without colon
-		Dim segments() As Byte = {DIGITS(4), DIGITS(2), DIGITS(2), DIGITS(3)}
+        Dim segments() As Byte = {DIGITS(4), DIGITS(2), DIGITS(2), DIGITS(3)}
         sd4x7.SetSegments(segments, 7, false)
 
         System.Console.WriteLine("Press key to exit")
-        System.Console.ReadKey()
+        System.Console.ReadLine()
         ipcon.Disconnect()
     End Sub
 End Module
