afe2efed32cd5dccc265b4b403a2d471ef459268
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 16:14:17 +0200

    Cleanup Perl example

--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletSegmentDisplay4x7;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'ioiu'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my @digits = (0x3f,0x06,0x5b,0x4f,
               0x66,0x6d,0x7d,0x07,
@@ -13,7 +13,7 @@ my @digits = (0x3f,0x06,0x5b,0x4f,
               0x39,0x5e,0x79,0x71); # // 0~9,A,b,C,d,E,F
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $sd4x7 = Tinkerforge::BrickletSegmentDisplay4x7->new(&UID, $ipcon); # Create device object
+my $sd = Tinkerforge::BrickletSegmentDisplay4x7->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
@@ -26,7 +26,7 @@ push(@segments, $digits[2]);
 push(@segments, $digits[2]);
 push(@segments, $digits[3]);
 
-$sd4x7->set_segments(\@segments, 7, 0);
+$sd->set_segments(\@segments, 7, 0);
 
 print "Press any key to exit...\n";
 <STDIN>;
