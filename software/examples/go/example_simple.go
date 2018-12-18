7ee687cda661314bb3508931e6d8fd286a745a5a
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:24:53 +0100

    go: Change import path again.

--- software/examples/go/example_simple.go
+++ software/examples/go/example_simple.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/tinkerforge/go-api-bindings/ipconnection"
-    "github.com/tinkerforge/go-api-bindings/segment_display_4x7_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
+    "github.com/Tinkerforge/go-api-bindings/segment_display_4x7_bricklet"
 )
 
 const ADDR string = "localhost:4223"
